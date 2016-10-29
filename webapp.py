7db39794d8ce2c9c528d9e0fb3ccb698db70b467
Andre Miras <andre.miras@gmail.com>
2016-10-29 20:56:08 +0200

    Handle flash messages

--- webapp.py
+++ webapp.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+import os
+import random
+from flask import Flask, request, render_template, flash, redirect, url_for, session
 from forms import NanoTtsForm
 
 app = Flask(__name__)
@@ -12,6 +14,7 @@ def home():
         text = form.text.data
         # return redirect(url_for('play'))
         print "say it:", text
+        flash('Playing text')
     data = {
         "text": text,
         "form": form,
@@ -20,4 +23,9 @@ def home():
 
 
 if __name__ == '__main__':
+    secret_key = os.environ.get('SECRET_KEY')
+    # generates a one time secret key
+    if secret_key is None:
+        secret_key = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])
+    app.secret_key = secret_key
     app.run(port=8000, debug=True)
