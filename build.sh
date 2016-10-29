31e043f9060119fe5f221b3bc0d4562352e684eb
Andre Miras <andre.miras@gmail.com>
2016-10-29 18:32:51 +0200

    Update build.sh
     * chmod +x
     * patch Makefile

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+# builds nanotts for Heroku
 wget --continue 'https://github.com/gmn/nanotts/archive/master.zip'
 unzip master.zip -d /tmp/
+patch -d /tmp/nanotts-master/ < Makefile.patch
 cd /tmp/nanotts-master/
 make
