6d0f7a3899a0c5be0e5c05def5206ceb3b7fef57
Andre Miras <andre.miras@gmail.com>
2016-10-31 23:11:16 +0100

    Update build.sh
    Transfer binary to transfer.sh after build.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-# builds nanotts for Heroku
+# builds nanotts for Heroku and upload it to transfer.sh
 cd /tmp/
 wget -c 'https://github.com/gmn/nanotts/archive/master.zip'
 unzip master.zip
 cd /tmp/nanotts-master/
 patch < Makefile.patch
 make
+curl --upload-file ./nanotts https://transfer.sh/
