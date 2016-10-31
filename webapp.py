6ccf18f694b243e8cf3244bb2da4eb2d899aaa38
Andre Miras <andre.miras@gmail.com>
2016-10-31 22:15:47 +0100

    Update webapp.py & home.html
    First draft playing wav file.
    Deleting temp ones after 24h.

--- webapp.py
+++ webapp.py
@@ -1,22 +1,56 @@
 import os
 import random
+from datetime import datetime, timedelta
+from tempfile import NamedTemporaryFile
 from flask import Flask, request, render_template, flash, redirect, url_for, session
 from forms import NanoTtsForm
+from libnanotts import NanoTts
 
 app = Flask(__name__)
+AUDIO_FORMAT = ".wav"
 
+
+def audio_directory():
+    """
+    Returns the audio directory absolute path.
+    """
+    return os.path.join(app.static_folder, 'audio')
+
+def delete_old_audio_files(path, hours=24):
+    """
+    Looks in `path` for files older than `hours`
+    and delete them.
+    """
+    for filename in os.listdir(path):
+        if filename.endswith(AUDIO_FORMAT):
+            filepath = os.path.join(path, filename)
+            file_modified = datetime.fromtimestamp(os.path.getmtime(filepath))
+            if datetime.now() - file_modified > timedelta(hours=hours):
+                os.remove(filepath)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     text = None
+    audio_file = None
     form = NanoTtsForm(request.form)
     if request.method == 'POST' and form.validate():
         text = form.text.data
         # return redirect(url_for('play'))
         print "say it:", text
         flash('Playing text')
+        nanotts = NanoTts()
+        nanotts.noplay = True
+        # TODO: use tempfile module
+        f = NamedTemporaryFile(
+            suffix=AUDIO_FORMAT, dir=audio_directory(), delete=False)
+        f.close()
+        audio_file = os.path.join("audio", os.path.basename(f.name))
+        nanotts.output = f.name
+        nanotts.run(text)
+        delete_old_audio_files(audio_directory())
     data = {
         "text": text,
+        "audio_file": audio_file,
         "form": form,
     }
     return render_template('home.html', **data)
